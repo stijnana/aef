8fa9f8c13ab07c6d0dafd678ebeb1d8520d83545
stijnana <stijn.olthof@live.nl>
2016-03-30 14:00:14 +0200

    none

--- index.php
+++ index.php
@@ -15,11 +15,11 @@ $fb = new Facebook\Facebook([
 ]);
 
 try {
-    $response = $fb->get('/me?fields=id,name,bio,birthday,context,cover,currency,devices,education,email,favorite_athletes,favorite_teams,first_name,gender,hometown,inspirational_people,install_type,installed,interested_in,is_shared_login,is_verified,languages,last_name,link,locale,location,meeting_for,middle_name,name_format,payment_pricepoints,political,public_key,quotes,relationship_status,religion,security_settings,shared_login_upgrade_required_by,significant_other,sports,test_group,third_party_id,timezone,updated_time,verified,video_upload_limits,viewer_can_send_gift,website,work&debug=all');
+    $response = $fb->get('/me?fields=id,name,bio,birthday,context,cover,currency,devices,education,email,favorite_athletes,favorite_teams,first_name,gender,hometown,inspirational_people,install_type,installed,interested_in,is_shared_login,is_verified,languages,last_name,link,locale,location,meeting_for,middle_name,name_format,payment_pricepoints,political,public_key,quotes,relationship_status,religion,security_settings,shared_login_upgrade_required_by,significant_other,sports,test_group,third_party_id,timezone,updated_time,verified,video_upload_limits,viewer_can_send_gift,age_range,website,work&debug=all');
 
     $user = $response->getGraphObject();
     echo "<pre>";
-    var_dump($user);
+    echo ($user);
     echo "</pre>";
     exit;
 } catch (Facebook\Exceptions\FacebookResponseException $e){
@@ -29,7 +29,8 @@ try {
 }
 
 $helper = $fb->getRedirectLoginHelper();
-$permissions = ['email', 'user_likes'];
-$loginUrl = $helper->getLoginUrl('http://localhost/leerjaar2/periode3/proj/login-callback.php', $permissions);
+//$permissions = ['email','user_likes'];
+$permissions = [];
+$loginUrl = $helper->getLoginUrl('http://localhost/leerjaar2/periode3/proj/aef/login-callback.php', $permissions);
 
 echo '<a href="'. $loginUrl . '">Log in with Facebook!</a>';
