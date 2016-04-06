6eb2fbe7e03095df7ba58ce3a56099d5d292ebf6
Daan van Dalen <StudioOfDan@hotmail.com>
2016-04-06 13:31:48 +0200

    version 1

--- logout.php
+++ logout.php
@@ -1,7 +1,15 @@
 <?php
 session_start();            //start session
+include 'views/header.php';
 $_SESSION = array();    //clear session array
 session_destroy();      //destroy session
-header( "refresh:2;url=index.php" );
-echo 'you are logged out';
+header( "refresh:3;url=index.php" );
 ?>
+<div class="container-fluid no-padding">
+    <div class="wrap2">
+        <div class="hwrap2">
+        <h1 class=" h1aef wow fadeInUp" data-wow-duration="2s" data-wow-delay="0s">You are logged out</h1>
+        </div>
+        <p >you are getting redirected to the loginpage</p>
+    </div>
+</div>
